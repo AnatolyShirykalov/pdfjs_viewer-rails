4122be9006f86be2727bfc5a52e9997c56388b15
Anatoly Shirykalov <pipocavsobake@gmail.com>
2017-08-08 13:21:57 +0300

    x-frame-options

--- app/controllers/pdfjs_viewer/application_controller.rb
+++ app/controllers/pdfjs_viewer/application_controller.rb
@@ -1,4 +1,11 @@
 module PdfjsViewer
   class ApplicationController < ActionController::Base
+    after_action :allow_iframe
+
+  private
+
+    def allow_iframe
+      response.headers.except! 'X-Frame-Options'
+    end
   end
 end
